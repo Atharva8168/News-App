246a3a156a894c2ce0fb0267081bbf67bc62a7f9
Atharva <bauskaratharva@gmail.com>
2021-09-26 00:24:02 +0530

    Recycler View Adapter Added

--- app/src/main/java/com/example/android/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/android/newsapp/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.android.newsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recycler_view.layoutManager = LinearLayoutManager(this)
     }
 }
---
+++ app/src/main/java/com/example/android/newsapp/NewsListAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.android.newsapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class NewsListAdapter (private val items : ArrayList<String>) : RecyclerView.Adapter<NewsHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_news, parent, false)
+        return NewsHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: NewsHolder, position: Int) {
+        val currentItem = items[position]
+        holder.titleView.text = currentItem
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+
+}
+
+class NewsHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val titleView : TextView = itemView.findViewById(R.id.title)
+
+}
